640480f28496012f07e30be79600255cfbac1cfc
ameenthaha <ameenthaha@ameenthahas-MacBook-Air.local>
2023-09-28 01:51:15 +0530

    1st

---
+++ react1/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ClassComponent from './ClassComponent';
+import FunctionalComponent from './FunctionalComponent';
+
+function App() {
+  return (
+    <div>
+      <h1>My React App</h1>
+      <ClassComponent />
+      <FunctionalComponent />
+    </div>
+  );
+}
+
+export default App;
